fa3482c1d8fce60bc3d260e6aaa854fb10cc96b1
ngoestupid <85279776+ngoestupid@users.noreply.github.com>
2022-11-07 23:33:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Bích Hâm';
+const nameGirl = 'Duyn Hâm';
 const giftUrl = 'http://nodemy.vn';
 const eventName = '.';
 const titleCard = '.';
-const contentCard = 'và đây là Hồng Bích người con gái hiền nhất tôi biết';
+const contentCard = 'và đây là Duyn người con gái 37';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'Duyn.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
