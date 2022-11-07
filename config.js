76766209a32e05c13ecea1619e4723cf1b0aabe1
ngoestupid <85279776+ngoestupid@users.noreply.github.com>
2022-11-07 23:43:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'và đây là Duyn người con gái 37';
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'duyn.png';
+const giftImage = 'Duynne.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
